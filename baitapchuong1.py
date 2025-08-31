2ff7ce650f21dae9f9ff8297309fd917ce5c1033
DTH235751_NEANGRA <neangra2765@gmail.com>
2025-09-01 00:10:46 +0700

    Add files via upload

---
+++ baitapchuong1.py
@@ -0,0 +1,23 @@
+# cau3,cac VD
+print("Hello World!")
+
+print('Alber Einstein đã từng nói: Một người không bao giờ mắc sai lầm không bao giờ thử bất kỳ điều gì mới.')
+
+if 5 > 2:
+    print('Five is greater than two!')
+
+total = 1 + \
+        2 + \
+        3
+print(total); print('Hello Python!')s
+
+if True:
+    print('True')
+else:
+    print('False')
+
+# cau4
+print("Chao cac ban !")
+# cau5
+name=input("Moi ban nhap ho va ten: ")
+print("Xin chao: ",name)
