d1a23d591ab8c162e0559e999151d098472d9bf2
neangradth235751 <neangra2765@gmail.com>
2025-09-26 22:09:48 +0700

    cau6chuong2

---
+++ bai6chuong2.py
@@ -0,0 +1,17 @@
+'''
+- Toán tử số học:
+/	:Chia lấy kết quả thực (float)	vd: 7 / 2	kq=3.5
+//	:Chia lấy phần nguyên	vd: 7 // 2	kq=3
+%	:Chia lấy phần dư	vd: 7 % 2	kq=1
+**	:Lũy thừa	vd: 2 ** 3	kq=8
+
+- Toán tử logic:
+and	:Trả về True nếu cả hai điều kiện đúng	VD: True and False	kq=False
+or	:Trả về True nếu ít nhất một điều kiện đúng	VD:True or False	kq=True
+
+Toán tử so sánh đối tượng:
+is	:Kiểm tra hai biến có trỏ đến cùng một đối tượng trong bộ nhớ hay không	VD: a is b	 kq= True nếu a và b là cùng một đối tượng
+*Lưu ý: is khác với ==. == so sánh giá trị, còn is so sánh vị trí trong bộ nhớ.
+
+
+'''
